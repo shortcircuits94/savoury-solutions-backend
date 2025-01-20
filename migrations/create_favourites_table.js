dce419bf811f294cf6cac67261d319f8c1b98fa3
Rebecca Scott <62228611+shortcircuits94@users.noreply.github.com>
2025-01-20 15:54:56 -0500

    updated migrations

--- migrations/create_favourites_table.js
+++ migrations/create_favourites_table.js
@@ -1,19 +1,27 @@
 export async function up(knex) {
-  await knex.schema.createTable("favourites", (table) => {
-    table.increments("id").primary();
-    table.integer("user_id").unsigned().notNullable();
-    table
-      .foreign("user_id")
-      .references("id")
-      .inTable("users")
-      .onDelete("CASCADE");
-    table.string("recipe_id").notNullable();
-    table.string("recipe_name").notNullable();
-    table.text("recipe_image").notNullable();
-    table.timestamps(true, true);
-  });
+  await knex.schema
+    .createTable("favourites", (table) => {
+      table.increments("id").primary();
+      table.integer("user_id").unsigned().notNullable();
+      table
+        .foreign("user_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      table.string("recipe_id").notNullable();
+      table.string("recipe_name").notNullable();
+      table.text("recipe_image").notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable("users", (table) => {
+      table.increments("id").primary();
+      table.string("name").notNullable();
+      table.string("email").unique().notNullable();
+      table.string("password").notNullable();
+      table.timestamps(true, true);
+    });
 }
 
 export async function down(knex) {
-  await knex.schema.dropTable("favourites");
+  await knex.schema.dropTable("users").dropTable("favourites");
 }
