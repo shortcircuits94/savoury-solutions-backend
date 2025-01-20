7f50e612084e71a0394cfe8cbf6f257a78e07052
Rebecca Scott <62228611+shortcircuits94@users.noreply.github.com>
2025-01-20 17:50:54 -0500

    updated cors

--- server/app.js
+++ server/app.js
@@ -15,6 +15,13 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://savoury-solutions.netlify.app", // Your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.options("*", cors());
 app.options("*", cors(corsOptions));
 
