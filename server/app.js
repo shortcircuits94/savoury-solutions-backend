27bf315400c6a7b600f0632020f8288f9fb8a774
Rebecca Scott <62228611+shortcircuits94@users.noreply.github.com>
2025-01-20 17:52:17 -0500

    cors issue

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(
   cors({
-    origin: "https://savoury-solutions.netlify.app", // Your frontend URL
+    origin: "https://savoury-solutions.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
